284e4e296b941d74030d3db5a8c15635d7f568d0
Yanjiun Chen <yjchen@Yanjiuns-Air.home>
2016-12-04 17:40:19 -0500

    working canvas drawing

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
-  	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/indexCanvas.html');
 });
 
-io.on('connection',function(socket){
+io.sockets.on('connection',function(socket){
     console.log('a user connected');
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+    socket.on('mousemove', function(data){
+        socket.broadcast.emit('moving', data);
     });
-    socket.on('disconnect',function(){
-        console.log('user disconnected');
-    });
+    //socket.on('chat message', function(msg){
+    //    io.emit('chat message', msg);
+    //});
+    //socket.on('disconnect',function(){
+     //   console.log('user disconnected');
+    //});
 });
 
-http.listen(3000, '0.0.0.0', function(){
+http.listen(3000,"0.0.0.0", function(){
 	console.log('listening on *:3000');
 });
 
